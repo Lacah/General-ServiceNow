3250efa1fd4f27d34d610f961b3f1605af734f8f
Lacah <47461634+Lacah@users.noreply.github.com>
2021-10-03 19:24:18 +0200

    Update ExcelParser.js

--- Script Includes/ExcelParser.js
+++ Script Includes/ExcelParser.js
@@ -1,5 +1,7 @@
 var ExcelParser = Class.create();
-ExcelParser.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+ExcelParser.prototype = {
+    initialize: function() {
+    },
 
     readData: function(attachmentID) {
         //Initialize the GlideSysAttachment call for retrieval of our sheet
@@ -10,16 +12,16 @@ ExcelParser.prototype = Object.extendsObject(AbstractAjaxProcessor, {
         var parser = new sn_impex.GlideExcelParser();
         parser.parse(attachmentStream);
         parser.setHeaderRowNumber(1);
-        //     var headers = parser.getColumnHeaders(); // Array of headers
-             var excelData = []; // An array that will hold the data from each line as an object
+        var headers = parser.getColumnHeaders(); // Array of headers
+        var excelData = []; // An array that will hold the data from each line as an object
 
         while (parser.next()) {
             var row = parser.getRow();
             if (JSUtil.notNil(row))
                 excelData.push(row);
-        }	
-		return excelData;
+        }
+        return excelData;
     },
 
     type: 'ExcelParser'
-});
+};

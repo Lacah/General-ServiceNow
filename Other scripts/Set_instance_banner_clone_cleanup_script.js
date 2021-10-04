223bfc0ed0be1f21de02e73696a10109cb8820df
Lacah <47461634+Lacah@users.noreply.github.com>
2021-10-04 19:56:42 +0200

    Update Set_instance_banner_clone_cleanup_script.js

--- Other scripts/Set_instance_banner_clone_cleanup_script.js
+++ Other scripts/Set_instance_banner_clone_cleanup_script.js
@@ -1,6 +1,24 @@
+/**
+* You can add this as clean-up script to a clone profile or run as fix script, background script etc. manually on the target instance after cloning.
+*
+*
+* Prerequisites:
+*
+* 1) You need to have the target instance's banner image attached to a record on the source system,  
+* e.g. have a knowledge article in production with the banner attached to it.
+*
+* 2) Make sure the table above mentioned table and record and included in your clone!
+*
+* 3) Set the source table name and the source record's sys_id as values for srcTbl and srcRec variables in the below section!
+**/
+
+/** MAKE SURE TO SET THESE 2 VARIABLES **/
+var srcTbl = ''; // TABLE NAME, e.g. kb_knowledge
+var srcRec = ''; // SYS ID of the record that you have attached the target instance's banner
+
 // Copy attachment from KBA
-GlideSysAttachment.copy('kb_knowledge','61d05fd7db9ae450f4db16d15b96190d ','sys_properties','3458de2aff7102007729ffffffffff7c'); // glide.product.image.light
-GlideSysAttachment.copy('kb_knowledge','61d05fd7db9ae450f4db16d15b96190d ','sys_properties','71e1b8dac0a8016a01ea6a1ca634c46d'); // glide.product.image
+GlideSysAttachment.copy(srcTbl,srcRec,'sys_properties','3458de2aff7102007729ffffffffff7c'); // glide.product.image.light
+GlideSysAttachment.copy(srcTbl,srcRec,'sys_properties','71e1b8dac0a8016a01ea6a1ca634c46d'); // glide.product.image
 
 // Get attachment sys_ids
 var lightIMG = new GlideRecord('sys_attachment');
@@ -9,7 +27,7 @@ lightIMG.setLimit(1);
 lightIMG.query();
 var lightIMGID = '';
 while(lightIMG.next()){
-lightIMGID = lightIMG.sys_id;
+  lightIMGID = lightIMG.sys_id;
 }
 
 var IMG = new GlideRecord('sys_attachment');
@@ -18,10 +36,10 @@ IMG.setLimit(1);
 IMG.query();
 var IMGID = '';
 while(IMG.next()){
-IMGID = IMG.sys_id;
+  IMGID = IMG.sys_id;
 }
 
-// set attachment sys_id in respective properties
+// Set attachment sys_id in respective properties
 var propLightIMG = new GlideRecord('sys_properties');
 propLightIMG.get('3458de2aff7102007729ffffffffff7c');
 propLightIMG.setValue('value',lightIMGID + '.iix');
